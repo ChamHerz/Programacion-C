a7bca2e302fedd014082f8a08d4902348e7ad949
ChamHerz <adrielchambers@gmail.com>
2018-09-07 18:55:07 -0300

    tp4

---
+++ rest/Libros/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html;charset=ISO-8859-1">
+	<title>Destino</title>
+	<link href="style.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+	<?php
+		echo "pagina libro<br/>";
+	?>
+</body>
+</html>
